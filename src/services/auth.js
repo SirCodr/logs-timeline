bfdf217f8c74faae79e7b91542c8d832e4ad32cf
SirCodr <jotadeveloper00@gmail.com>
2023-10-24 22:10:09 -0500

    Add auth services

---
+++ src/services/auth.js
@@ -0,0 +1,8 @@
+import { supabase } from '../supabaseClient'
+
+export const loginWithGmail = async () =>
+  await supabase.auth.signInWithOAuth({
+    provider: 'google'
+  })
+
+export const logout = async () => await supabase.auth.signOut()
