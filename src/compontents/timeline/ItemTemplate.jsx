8e78b2dd37c8ac385a60a21f12d984aadac2318f
SirCodr <jotadeveloper00@gmail.com>
2023-10-29 00:45:08 -0500

    Add card style for each log in time line

--- src/compontents/timeline/ItemTemplate.jsx
+++ src/compontents/timeline/ItemTemplate.jsx
@@ -1,14 +1,22 @@
+import { Card } from 'primereact/card'
+import { Badge } from 'primereact/badge'
+
 const ItemTemplate = ({ item }) => {
   const { groupLabel, groupValue, items } = item
-  return <div>
-    <p>{groupValue}</p>
-    {
-      items.map((item, index) => (
-        <p key={index}>
-          {item.title}
-        </p>
-      ))
-    }
-    </div>
+  return (
+    <Card>
+      <p className='mb-3'>
+        <strong>{groupValue}</strong>
+      </p>
+      <ul className='flex flex-col gap-y-3'>
+        {items.map((item, index) => (
+          <li key={index} className='flex items-baseline gap-x-2'>
+            <Badge className='bg-black' />
+            {item.title}
+          </li>
+        ))}
+      </ul>
+    </Card>
+  )
 }
 export default ItemTemplate
