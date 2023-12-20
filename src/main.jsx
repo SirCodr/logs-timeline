bd72b786662bdbe3f0dfe8ddc50930bfb9ab9bab
SirCodr <jotadeveloper00@gmail.com>
2023-12-20 16:08:51 -0500

    Add closeButton prop to Toaster component

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Toaster richColors />
+      <Toaster richColors closeButton />
     </QueryClientProvider>
   </React.StrictMode>,
 )
