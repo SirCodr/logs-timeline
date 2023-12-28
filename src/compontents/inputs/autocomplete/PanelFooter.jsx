7b56b20277bb375d927fac0eddacb1e52a25e41a
SirCodr <jotadeveloper00@gmail.com>
2023-12-27 23:04:47 -0500

    Refactor panelFooter rendering

--- src/compontents/inputs/autocomplete/PanelFooter.jsx
+++ src/compontents/inputs/autocomplete/PanelFooter.jsx
@@ -1,16 +1,17 @@
-import { Button } from "primereact/button"
-import useLogCategory from "../../../hooks/useLogCategory"
-import { useEffect } from "react"
+import { Button } from 'primereact/button'
+import useLogCategory from '../../../hooks/useLogCategory'
+import { useEffect } from 'react'
 
-const PanelFooter = ({items = [], selectedItem, onCreated = () => {}}) => {
-  const { handleChange, handleLogCategoryCreation, isLogCategoryCreating } = useLogCategory()
-  const isItemSelected = (items).some(
-    (item) => item['name'] === selectedItem
-  )
+const PanelFooter = ({ items = [], selectedItem, onCreated = () => {} }) => {
+  const { handleChange, handleLogCategoryCreation, isLogCategoryCreating } =
+    useLogCategory()
+  const isItemSelected = items.some((item) => item['name'] === selectedItem)
 
   useEffect(() => {
     handleChange({ target: { value: selectedItem } })
   }, [selectedItem])
+
+  if (!items.length && !selectedItem) return <></>
 
   return (
     <div className='py-2 px-3'>
@@ -19,11 +20,10 @@ const PanelFooter = ({items = [], selectedItem, onCreated = () => {}}) => {
           <b>{selectedItem}</b> selected.
         </span>
       )}
-      {Boolean(items.length) &&
-        !isItemSelected &&
-        'Aún no has seleccionado.'}
-      {!items.length && !isItemSelected && (
-        <Button 
+      {Boolean(items.length) && !isItemSelected && 'Aún no has seleccionado.'}
+
+      {!items.length && selectedItem && Boolean(selectedItem.length) && (
+        <Button
           type='button'
           label='Crear nuevo'
           loading={isLogCategoryCreating}
