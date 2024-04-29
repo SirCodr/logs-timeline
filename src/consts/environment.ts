090bc2bf7116398a7de19578ec59f19a2b5b7e14
juan.restrepooquendo <juan.restrepooquendo@publicisgroupe.com>
2024-04-29 09:41:33 -0500

    Add dev mode and implement prod api url

---
+++ src/consts/environment.ts
@@ -0,0 +1,1 @@
+export const IS_DEV_MODE = import.meta.env.VITE_LTL_IS_DEV_MODE === 'TRUE' ? true : false
--- src/https/index.ts
+++ src/https/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import { IS_DEV_MODE } from '../consts/environment'
 
 const http = axios.create()
+console.log(IS_DEV_MODE);
 
-http.defaults.baseURL = 'http://localhost:3000'
+http.defaults.baseURL = IS_DEV_MODE ? 'http://localhost:3000' : 'https://logs-timeline-api.vercel.app/'
 
 export default http
